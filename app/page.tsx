1a4f6cdeac8962d4ce352df6a97eef46ac2a3a67
Santtamk <santtamk@gmail.com>
2025-08-31 11:08:03 +0530

    image still not working

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import NavBar from "./Components/NavBar";
 
-
 import RippleGrid from "./Components/RippleGrid";
 import FlowingMenu from "./Components/FlowingMenu";
 import OurExpertise from "./Components/OurExpertise";
@@ -14,20 +13,11 @@ import ExcellentaFormsStack from "./Components/Form";
 import LogoComp from "./Components/logoLoop/LogoComp";
 // import TitleCardMap from "./Components/services/TitleCardMap";
 
-
 export default function Home() {
   return (
     <main className="relative h-screen bg-[#060010]">
       {/* Background image/distortion (bottom layer) */}
       <div className="absolute inset-0 z-0 h-screen max-w-[1300px] mx-auto">
-        {/* <GridDistortion
-          imageSrc="/hero02.png"
-          grid={10}
-          mouse={0.1}
-          strength={0.15}
-          relaxation={0.9}
-          className="w-full h-full"
-        /> */}
         <RippleGrid
           enableRainbow={false}
           gridColor="#5227ff"
@@ -51,12 +41,11 @@ export default function Home() {
       <section className="relative z-20 grid h-full lg:place-items-center pointer-events-none ">
         {/* put CTA/title here; remove pointer-events-none to make them clickable */}
         {/* <h1 className="text-white text-5xl font-bold">@1Stop</h1> */}
-        <h1 className="text-8xl font-bold ">
+        <h1 className="text-8xl font-bold text-center max-w-[950px]">
           {" "}
           <span className="font-serif italic   px-6">from</span>
-          Pixel
-          <span className="font-serif px-6 italic ">to</span>
-          Audience{" "}
+          Field to Screen<span className="font-serif px-6 italic ">—</span>
+          As It Happens{" "}
         </h1>
       </section>
 
